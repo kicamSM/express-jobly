7f58ae7b453f657f4f3f2e19968b7f1b8f22b7d0
kicamSM <kicamSMM@hotmail.com>
2023-08-11 11:08:28 -0600

    Tests for sql.test.js

--- helpers/sql.js
+++ helpers/sql.js
@@ -24,3 +24,15 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
 }
 
 module.exports = { sqlForPartialUpdate };
+
+// function sqlForPartialUpdate(dataToUpdate, jsToSql) { 
+//   const keys = Object.keys(dataToUpdate);
+//   if (keys.length === 0) throw new BadRequestError("No data");
+//   const cols = keys.map((colName, idx) =>
+//   `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+// );
+// return {
+//   setCols: cols.join(", "),
+//   values: Object.values(dataToUpdate),
+// };
+// }
---
+++ helpers/sql.test.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const { createToken, sqlForPartialUpdate } = require("./sql");
+const { SECRET_KEY } = require("../config");
+const { BadRequestError } = require("../expressError");
+
+
+describe("sqlForPartialUpdate", function () {
+    test("works", function () {
+
+    const dataToUpdate = {first_name: 'Aliya', age: 32}
+    //   expect(col).toEqual(['"first_name"=$1', '"age"=$2']);
+    const jsToSql = {first_name: "firstName"}
+    let sql = sqlForPartialUpdate(dataToUpdate, jsToSql)
+    expect(sql).toEqual({
+        setCols: "\"firstName\"=$1, \"age\"=$2",
+        values: ["Aliya", 32] });
+});
+
+test("Returns error for empty object", function () {
+
+    const dataToUpdate = {}
+    try {
+        let sql = sqlForPartialUpdate(dataToUpdate)
+        console.log("sql:", sql)
+        // expect(sql).toEqual(BadRequestError)
+    } catch (err) {
+        expect(err).toBeInstanceOf(BadRequestError)
+        expect(err.message).toEqual("No data")
+    }
+    });
+
+})
+
