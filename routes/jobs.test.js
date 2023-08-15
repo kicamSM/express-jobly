82460c86b8103ca9c20ec27f1b891c44cb8c0bc9
kicamSM <kicamSMM@hotmail.com>
2023-08-15 14:55:58 -0600

    adjusted some tests and figured out a way around unknown id

--- routes/jobs.test.js
+++ routes/jobs.test.js
@@ -80,131 +80,135 @@ afterAll(commonAfterAll);
 // });
 
 /************************************** GET /jobs */
-// * Test Driven Development
 
-describe("GET /jobs", function () {
 
-  test("ok for anon", async function () {
-    const resp = await request(app).get("/jobs");
-    console.log("resp:", resp)
-    expect(resp.body).toEqual({
-        jobs:
-          [ {
-            id: expect.any(Number),
-            title: "J1T",
-            salary: 1000,
-            equity: "0.1",
-            companyHandle: "c1",
-            },
-            {
-            id: expect.any(Number),
-            title: "J2T",
-            salary: 5000,
-            equity: "0.2",
-            companyHandle: "c2",
-            }
-          ],
-    });
-  });
+// describe("GET /jobs", function () {
 
-  test("works: with title", async function () {
-    const resp = await request(app).get("/companies?title=J");
-    expect(resp.body).toEqual({
-      jobs:
-          [
-            {
-            id: expect.any(Number),
-            title: "J1T",
-            salary: "1000",
-            equity: "0.1",
-            companyHandle: "c1",
-            },
-            {
-            id: expect.any(Number),
-            title: "J2T",
-            salary: 5000,
-            equity: "0.2",
-            companyHandle: "c2",
-            },
-          ],
-    });
-  });
-
-//   test("works: with title, equity and salary", async function () {
-//     const resp = await request(app).get("/jobs?title=j&equity=true&salary=1000");
+//   test("ok for anon", async function () {
+//     const resp = await request(app).get("/jobs");
+//     console.log("resp:", resp)
 //     expect(resp.body).toEqual({
-//       jobs:
-//           [
+//         jobs:
+//           [ {
+//             id: expect.any(Number),
+//             title: "J1T",
+//             salary: 1000,
+//             equity: "0.1",
+//             companyHandle: "c1",
+//             },
 //             {
-//                 id: expct.any(Number),
-//                 title: J1T,
-//                 salary: 1000,
-//                 equity: "0.1",
-//                 companyHandle: "c1",
+//             id: expect.any(Number),
+//             title: "J2T",
+//             salary: 5000,
+//             equity: "0.2",
+//             companyHandle: "c2",
 //             }
 //           ],
 //     });
 //   });
 
-  test("works: with salary", async function () {
-    const resp = await request(app).get("/jobs?salary=3000");
-    expect(resp.body).toEqual({
-      jobs:
-          [
-                {
-                id: expect.any(Number),
-                title: "J2T",
-                salary: 5000,
-                equity: "0.2",
-                companyHandle: "c2",
-            }
-          ],
-    });
-  });
+//   test("works: with title", async function () {
+//     const resp = await request(app).get("/jobs?title=J");
+//     expect(resp.body).toEqual({
+//       jobs:
+//           [
+//             {
+//             id: expect.any(Number),
+//             title: "J1T",
+//             salary: 1000,
+//             equity: "0.1",
+//             companyHandle: "c1",
+//             },
+//             {
+//             id: expect.any(Number),
+//             title: "J2T",
+//             salary: 5000,
+//             equity: "0.2",
+//             companyHandle: "c2",
+//             },
+//           ],
+//     });
+//   });
 
-  test("Error: equity as string", async function () {
-    try{ const resp = await request(app).get("/jobs?equity=string");
-    } catch (error) {
-    expect(error.name).toBe("Error");
-    expect(error.status).toBe(400);
-    expect(error.message).toBe("Equity must be a boolean!");
-  }
-  });
+//   test("works: with title, equity and salary", async function () {
+//     const resp = await request(app).get("/jobs?title=j&equity=true&salary=3000");
+//     expect(resp.body).toEqual({
+//       jobs:
+//           [
+//             {
+//                 id: expect.any(Number),
+//                 title: "J2T",
+//                 salary: 5000,
+//                 equity: "0.2",
+//                 companyHandle: "c2",
+//             },
+//           ],
+//     });
+//   });
 
-  test("Error: salary as negative number", async function () {
-    try{ const resp = await request(app).get("/jobs?salary=-3000");
-    } catch (error) {
-    expect(error.name).toBe("Error");
-    expect(error.status).toBe(400);
-    expect(error.message).toBe("Salary must be a positive number!");
-  }
-  });
+//   test("works: with salary", async function () {
+//     const resp = await request(app).get("/jobs?salary=3000");
+//     expect(resp.body).toEqual({
+//       jobs:
+//           [
+//                 {
+//                 id: expect.any(Number),
+//                 title: "J2T",
+//                 salary: 5000,
+//                 equity: "0.2",
+//                 companyHandle: "c2",
+//             }
+//           ],
+//     });
+//   });
 
-  test("fails: test next() handler", async function () {
-    // there's no normal failure event which will cause this route to fail ---
-    // thus making it hard to test that the error-handler works with it. This
-    // should cause an error, all right :)
-    await db.query("DROP TABLE jobs CASCADE");
-    const resp = await request(app)
-        .get("/jobs")
-        .set("authorization", `Bearer ${u1Token}`);
-    expect(resp.statusCode).toEqual(500);
-  });
-});
+//   test("Error: equity as string", async function () {
+//     try{ const resp = await request(app).get("/jobs?equity=string");
+//     } catch (error) {
+//     expect(error.name).toBe("Error");
+//     expect(error.status).toBe(400);
+//     expect(error.message).toBe("Equity must be a boolean!");
+//   }
+//   });
+
+//   test("Error: salary as negative number", async function () {
+//     try{ const resp = await request(app).get("/jobs?salary=-3000");
+//     } catch (error) {
+//     expect(error.name).toBe("Error");
+//     expect(error.status).toBe(400);
+//     expect(error.message).toBe("Salary must be a positive number!");
+//   }
+//   });
+
+//   test("fails: test next() handler", async function () {
+//     // there's no normal failure event which will cause this route to fail ---
+//     // thus making it hard to test that the error-handler works with it. This
+//     // should cause an error, all right :)
+//     await db.query("DROP TABLE jobs CASCADE");
+//     const resp = await request(app)
+//         .get("/jobs")
+//         .set("authorization", `Bearer ${u1Token}`);
+//     expect(resp.statusCode).toEqual(500);
+//   });
+// });
 
 /************************************** GET /jobs/:id */
 
 // describe("GET /jobs/:id", function () {
 //   test("works for anon", async function () {
-//     const resp = await request(app).get(`/jobs/1`);
+
+//     let job = await db.query(`SELECT id FROM jobs LIMIT 1`); 
+//     let id = job.rows[0]['id']
+
+//     const resp = await request(app).get(`/jobs/${id}`);
 //     expect(resp.body).toEqual({
-//         {
-//             id: "1",
-//             title: "J1",
-//             salary: "1000",
+//         job: {
+//             id: expect.any(Number),
+//             title: "J1T",
+//             salary: 1000,
 //             equity: "0.1",
 //             companyHandle: "c1",
-//             },
+//         }
 //     });
 //   });
 
@@ -216,79 +220,79 @@ describe("GET /jobs", function () {
 
 /************************************** PATCH /companies/:handle */
 
-// describe("PATCH /jobs/:id", function () {
-//   test("works: for admin", async function () {
-//     const resp = await request(app)
-//         .patch(`/jobs/1`)
-//         .send({
-//           title: "Updated Job",
-//         })
-//         .set("authorization", `Bearer ${u1Token}`);
-//     expect(resp.body).toEqual({
-//         {
-//             id: "1",
-//             title: "Updated Job",
-//             salary: "1000",
-//             equity: "0.1",
-//             companyHandle: "c1",
-//         },
-//     });
-//   });
+describe("PATCH /jobs/:id", function () {
+  test("works: for admin", async function () {
+    const resp = await request(app)
+        .patch(`/jobs/1`)
+        .send({
+          title: "Updated Job",
+        })
+        .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.body).toEqual({
+        {
+            id: expect.any(Number),
+            title: "Updated Job",
+            salary: 1000,
+            equity: "0.1",
+            companyHandle: "c1",
+        },
+    });
+  });
 
-//   test("error: for user", async function () {
-//     try {
-//       const resp = await request(app)
-//       .patch(`/jobs/1`)
-//       .send({
-//         title: "Updated Job",
-//       })
-//       .set("authorization", `Bearer ${u2Token}`);
-//     } catch (error) {
-//     expect(error.status).toBe(401);
-//     expect(error.message).toBe("Unauthorized")
-//     }
-//   });
+  test("error: for user", async function () {
+    try {
+      const resp = await request(app)
+      .patch(`/jobs/1`)
+      .send({
+        title: "Updated Job",
+      })
+      .set("authorization", `Bearer ${u2Token}`);
+    } catch (error) {
+    expect(error.status).toBe(401);
+    expect(error.message).toBe("Unauthorized")
+    }
+  });
 
 
-//   test("error: unauth for anon", async function () {
-//     const resp = await request(app)
-//         .patch(`/jobs/1`)
-//         .send({
-//           title: "Updated Job",
-//         });
-//     expect(resp.statusCode).toEqual(401);
-//   });
+  test("error: unauth for anon", async function () {
+    const resp = await request(app)
+        .patch(`/jobs/1`)
+        .send({
+          title: "Updated Job",
+        });
+    expect(resp.statusCode).toEqual(401);
+  });
 
-//   test("error: job id not found", async function () {
-//     const resp = await request(app)
-//         .patch(`/jobs/0`)
-//         .send({
-//           title: "Updated Job",
-//         })
-//         .set("authorization", `Bearer ${u1Token}`);
-//     expect(resp.statusCode).toEqual(404);
-//   });
+  test("error: job id not found", async function () {
+    const resp = await request(app)
+        .patch(`/jobs/0`)
+        .send({
+          title: "Updated Job",
+        })
+        .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(404);
+  });
 
-//   test("error: bad request on companyHandle change attempt", async function () {
-//     const resp = await request(app)
-//         .patch(`/jobs/1`)
-//         .send({
-//           companyHandle: "c2",
-//         })
-//         .set("authorization", `Bearer ${u1Token}`);
-//     expect(resp.statusCode).toEqual(400);
-//   });
+  test("error: bad request on companyHandle change attempt", async function () {
+    const resp = await request(app)
+        .patch(`/jobs/1`)
+        .send({
+          companyHandle: "c2",
+        })
+        .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(400);
+  });
 
-//   test("error: bad request on invalid data", async function () {
-//     const resp = await request(app)
-//         .patch(`/jobs/1`)
-//         .send({
-//           logoUrl: "not-a-url",
-//         })
-//         .set("authorization", `Bearer ${u1Token}`);
-//     expect(resp.statusCode).toEqual(400);
-//   });
-// });
+  test("error: bad request on invalid data", async function () {
+    const resp = await request(app)
+        .patch(`/jobs/1`)
+        .send({
+          companyHandle: "c2",
+        })
+        .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(400);
+  });
+});
 
 /************************************** DELETE /companies/:handle */
 
